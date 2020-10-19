4aedc926af45a1690e002f81c748ab9125244072
Nikita Kuznetsov <ndkuznetsv_1@edu.hse.ru>
2020-10-19 11:58:00 +0300

    Initial commit

---
+++ HookahProject/AppDelegate.swift
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.swift
+//  HookahProject
+//
+//  Created by Nikita Kuznetsov on 18.10.2020.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+}
+
