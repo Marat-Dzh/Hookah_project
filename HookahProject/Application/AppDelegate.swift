e59f903ee824859fb495c838b5e874f7e18a8b50
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2020-10-21 21:24:15 +0300

    damn now everything works. just clone and go

--- HookahProject/Application/AppDelegate.swift
+++ HookahProject/Application/AppDelegate.swift
@@ -13,6 +13,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        let window = UIWindow()
+        let vc = UIViewController()
+        vc.view.backgroundColor = .cyan
+        window.rootViewController = vc
+        window.makeKeyAndVisible()
+        self.window = window
         return true
     }
 
