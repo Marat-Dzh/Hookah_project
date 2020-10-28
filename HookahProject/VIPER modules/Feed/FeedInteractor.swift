88d37186256b35a6e9aef7e9773a340a54603c0f
Nikita Kuznetsov <ndkuznetsov_1@edu.hse.ru>
2020-10-28 20:00:33 +0300

    Structured Feed module

---
+++ HookahProject/VIPER modules/Feed/FeedInteractor.swift
@@ -0,0 +1,25 @@
+//
+//  FeedInteractor.swift
+//  HookahProject
+//
+//  Created by Nikita Kuznetsov on 28.10.2020.
+//
+
+import Foundation
+
+final class FeedInteractor{
+    weak var presenter: FeedInteractorOutput?
+    
+}
+
+extension FeedInteractor: FeedInteractorInput{
+    func getNewsFromServer() {
+        <#code#>
+    }
+    
+    func getStocksFromServer() {
+        <#code#>
+    }
+    
+    
+}
